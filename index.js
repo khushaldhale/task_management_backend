c74ab65be35c346bc74bbc85289be6f2c29b9835
khushaldhale <khushaldhale999@gmail.com>
2024-12-15 22:37:27 +0530

    cors and cookie midddleware set

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const express = require('express');
 const app = express();
 require("dotenv").config()
 
-app.use(express.json());
 
+
+//middlewares 
+app.use(express.json());
+const cors = require("cors");
+app.use(cors({
+	//  will modify the origin later 
+	origin: "*",
+	credentials: true
+}))
+const cookies = require("cookie-parser");
+app.use(cookies())
+
+
+
+// server check
 app.get('/', (req, res) => {
 	return res.status(200)
 		.json({
@@ -12,17 +26,16 @@ app.get('/', (req, res) => {
 		})
 })
 
-
+// db connection 
 const dbConnect = require("./config/database");
 dbConnect()
 
 // routes mapping 
-
 const authRoutes = require("./routes/authRoutes");
 app.use("/api/v1/auth", authRoutes)
 
-const PORT = process.env.PORT || 4001;
 
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
 	console.log("server is listening at : ", PORT)
 })
