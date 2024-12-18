df9324008b2897ec7ef31eb538e13efeea55d596
khushaldhale <khushaldhale999@gmail.com>
2024-12-18 17:36:48 +0530

    task status updates routes are done

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { authentication, isUser } = require("../middlewares/authentication");
-const { createTask, deleteTask, updateTask, getAllTask } = require("../controllers/task");
+const { createTask, deleteTask, updateTask, getAllTask, markAsComplete, markAsProgress, getParticularTask } = require("../controllers/task");
 const router = express.Router();
 
 
@@ -8,6 +8,11 @@ router.post("/", authentication, isUser, createTask);
 router.delete("/:id", authentication, isUser, deleteTask);
 router.put("/:id", authentication, isUser, updateTask);
 router.get("/", authentication, isUser, getAllTask);
+router.put("/:id/complete", authentication, isUser, markAsComplete);
+router.put("/:id/in-progress", authentication, isUser, markAsProgress);
+router.get("/:id", authentication, isUser, getParticularTask);
+
+
 
 
 module.exports = router
